ef09733d2a559070c58e34f67fdf3810de356543
stevenjlance <steven.j.lance@gmail.com>
2023-04-06 17:23:43 -0400

    updated starter code for tests

--- index.test.js
+++ index.test.js
@@ -1,3 +1,24 @@
 const { Comment, Like, Post, Profile, User } = require("./index");
+const { sequelize } = require('./db');
 
-// Write your tests here
+describe('Social Sequelzie Test', () => {
+    /**
+     * Runs the code prior to all tests
+     */
+    beforeAll(async () => {
+        // the 'sync' method will create tables based on the model class
+        // by setting 'force:true' the tables are recreated each time the 
+        // test suite is run
+        await sequelize.sync({ force: true });
+    })
+
+    // Write your tests here
+    
+    test("replace with your test", function() {
+        expect(true).toBe(true);
+    })
+
+
+
+
+})
